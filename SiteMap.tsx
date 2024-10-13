6d926bdd2a75c69279051b4720c60397d6e9418b
Hardik Upadhyay <hardik89990@gmail.com>
2024-10-14 00:00:36 +0530

    SiteMap.tsx

---
+++ SiteMap.tsx
@@ -0,0 +1,103 @@
+import { useState, useEffect, useRef } from 'react';
+import {
+  APIProvider,
+  ControlPosition,
+  MapControl,
+  AdvancedMarker,
+  Map,
+  useMap,
+  useMapsLibrary,
+  useAdvancedMarkerRef
+} from '@vis.gl/react-google-maps';
+
+const SiteMap = () => {
+  const [selectedPlace, setSelectedPlace] = useState<google.maps.places.PlaceResult | null>(null);
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAP_API_KEY;
+  console.log(API_KEY)
+  return (
+    <div style={{ width: '100%', height: '400px' }}> 
+      <APIProvider
+        apiKey={API_KEY}
+        libraries={['places']} 
+        solutionChannel='GMP_devsite_samples_v3_rgmautocomplete'
+      >
+        <Map
+          mapId={'bf51a910020fa25a'}
+          defaultZoom={3}
+          defaultCenter={{ lat: 22.54992, lng: 0 }}
+          gestureHandling={'greedy'}
+          disableDefaultUI={true}
+          style={{ width: '100%', height: '100%' }} // Add size to Map
+        >
+          <AdvancedMarker
+            ref={markerRef}
+            position={{ lat: 22.54992, lng: 0 }} // Set initial position
+          />
+          <MapControl position={ControlPosition.TOP}>
+            <div className="autocomplete-control">
+              <PlaceAutocomplete onPlaceSelect={setSelectedPlace} />
+            </div>
+          </MapControl>
+        </Map>
+        <MapHandler place={selectedPlace} marker={marker} />
+      </APIProvider>
+    </div>
+  );
+};
+
+interface MapHandlerProps {
+  place: google.maps.places.PlaceResult | null;
+  marker: google.maps.marker.AdvancedMarkerElement | null;
+}
+
+const MapHandler = ({ place, marker }: MapHandlerProps) => {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!map || !place || !marker) return;
+
+    if (place.geometry?.viewport) {
+      map.fitBounds(place.geometry?.viewport);
+    }
+    marker.position = place.geometry?.location;
+  }, [map, place, marker]);
+
+  return null;
+};
+
+interface PlaceAutocompleteProps {
+  onPlaceSelect: (place: google.maps.places.PlaceResult | null) => void;
+}
+
+const PlaceAutocomplete = ({ onPlaceSelect }: PlaceAutocompleteProps) => {
+  const [placeAutocomplete, setPlaceAutocomplete] = useState<google.maps.places.Autocomplete | null>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const places = useMapsLibrary('places');
+
+  useEffect(() => {
+    if (!places || !inputRef.current) return;
+
+    const options = {
+      fields: ['geometry', 'name', 'formatted_address']
+    };
+
+    setPlaceAutocomplete(new places.Autocomplete(inputRef.current, options));
+  }, [places]);
+
+  useEffect(() => {
+    if (!placeAutocomplete) return;
+
+    placeAutocomplete.addListener('place_changed', () => {
+      onPlaceSelect(placeAutocomplete.getPlace());
+    });
+  }, [onPlaceSelect, placeAutocomplete]);
+
+  return (
+    <div className="autocomplete-container">
+      <input ref={inputRef} placeholder="Search for a place" className='absolute ' />
+    </div>
+  );
+};
+
+export default SiteMap;
